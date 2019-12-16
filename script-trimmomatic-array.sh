65b9d0c5dd3b8be4bf17fe04daf714318711fd0a
o-william-white <o.william.white@gmail.com>
2019-12-16 10:03:05 +0000

    Update script-trimmomatic-array.sh

--- script-trimmomatic-array.sh
+++ script-trimmomatic-array.sh
@@ -4,6 +4,7 @@
 #$ -pe smp 1
 #$ -l h_vmem=1G
 #$ -t 1-283
+#$ -c 36
 #$ -N job-trimmomatic-array
 
 INPUT_FILE=$(sed -n "${SGE_TASK_ID}p" /data/scratch/mpx469/sample-list.txt)
@@ -12,9 +13,11 @@ module load trimmomatic/0.36
 
 java -jar /share/apps/centos7/trimmomatic/0.36/trimmomatic-0.36.jar  \
    SE \
-   /data/scratch/mpx469/Data2Bio_final/raw/$INPUT_FILE'.digested.fq.gz' \
-   /data/scratch/mpx469/trimmomatic/trimmomatic-output/$INPUT_FILE'.digested.trimmomatic.fq.gz' \
+   /data/scratch/mpx469/Data2Bio_final/raw/${INPUT_FILE}.digested.fq.gz \
+   /data/scratch/mpx469/trimmomatic/trimmomatic-output/${INPUT_FILE}.trimmomatic.fq.gz \
+   LEADING:15 \
    TRAILING:15 \
    SLIDINGWINDOW:4:15 \
    MINLEN:36
+
 
